6f2ebac591a1418c205329db072b4817ae612af6
jannikjw <jayjay1110@hotmail.de>
2022-05-05 18:45:26 +0000

    Refactored Code. Fixed subset_labels. Added BERT model.

---
+++ utils/.ipynb_checkpoints/data_loader-checkpoint.py
@@ -0,0 +1,106 @@
+# +
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+class DataLoader():
+    '''
+    Class to handle all data-related tasks
+    '''
+    def __init__(self, data_path="../geolifeclef-2022-lifeclef-2022-fgvc9/"):
+        # let's load the data from file
+        self.data_path = Path(data_path)
+        
+        df_obs_fr = pd.read_csv(self.data_path / "observations" / "observations_fr_train.csv", sep=";", index_col="observation_id")
+        df_obs_us = pd.read_csv(self.data_path / "observations" / "observations_us_train.csv", sep=";", index_col="observation_id")
+        self.df_obs = pd.concat((df_obs_fr, df_obs_us))
+
+    def subset_labels(self, set_type, num_labels=10):
+        '''
+        Select only a subset of labels
+        Returns: observation
+        '''
+        if set_type not in ['train', 'val']: 
+            return 'You need to choose one of the set types "train" or "val".'
+
+        df_obs = self.df_obs
+        # select the ids from the given subset
+        obs_ids = df_obs.index[df_obs["subset"] == set_type].values
+        labels = df_obs.loc[obs_ids]["species_id"].values       
+
+        obs_list = list()
+
+        # iterate over a subset of the labels
+        counter = 0
+        for label in np.unique(labels)[:num_labels]:
+            # for each label, retrieve all corresponding observation ids
+            obs = df_obs[(df_obs["species_id"] == label) & (df_obs["subset"] == set_type)].index.values
+            obs_list.append(obs)
+
+        # we now have a numpy array of all observation ids corresponding to this subset of labels
+        observation_ids = np.concatenate(obs_list)
+
+        # obtain the labels in the right order 
+        labels = df_obs.loc[observation_ids]["species_id"].values
+
+        return observation_ids, labels
+    
+    def load_environmental_data(self, train_ids, val_ids):
+        df_obs_bio = pd.read_csv(self.data_path / "pre-extracted" / "environmental_vectors.csv", sep=";", index_col="observation_id")
+        df_environmental_vars = pd.read_csv(self.data_path / "metadata" / "environmental_variables.csv", sep=";", index_col="name")
+
+        # collect gps data for selected observations
+        gps_data_train = df_obs.loc[train_ids][['latitude', 'longitude']]
+        gps_data_val = df_obs.loc[val_ids][['latitude', 'longitude']]
+
+        # collect biological and pedologic data
+        bio_data_train = df_obs_bio.loc[train_ids]
+        bio_data_val = df_obs_bio.loc[val_ids]
+
+        # join environmental data and GPS coordinates
+        df_train = bio_data_train.join(gps_data_train)
+        df_val = bio_data_val.join(gps_data_val)
+                
+        return df_train, df_val
+    
+def rename_environmental_table(self, df):
+    # rename columns of joined table with meaningful descriptions        
+    environmental_vars = list(df_environmental_vars['description'])
+    environmental_vars.append('latitude')
+    environmental_vars.append('longitude')
+
+    df.columns = environmental_vars
+
+    return df
+
+def transform_table_to_sentences(self, df):
+    # convert numerical values to strings 
+    df = df.astype(str)
+
+    # add column name as token before every cell value
+    for col in df.columns:
+        df[col] = df[col].map(lambda x: "<{}> {}".format(col, x))
+
+    # create "sentences" from table
+    df = list(df.stack().groupby(level=0).apply(' '.join))
+
+    return df
+
+def tokenize(sentences, tokenizer, max_length):
+    input_ids, input_masks, input_segments = [], [], []
+    for sentence in sentences:
+        inputs = tokenizer.encode_plus(sentence, add_special_tokens = True, max_length = max_length, pad_to_max_length = True, truncation=True, return_attention_mask = True, return_token_type_ids = True)
+        input_ids.append(inputs['input_ids'])
+        input_masks.append(inputs['attention_mask'])
+        input_segments.append(inputs['token_type_ids'])
+    return np.asarray(input_ids, dtype = "int32"), np.asarray(input_masks, dtype = "int32"), np.asarray(input_segments, dtype = "int32")
+
+    
+    def get_data_path(self):
+        return self.data_path
+    
+    def set_data_path(self, data_path):
+        self.data_path = Path(path)
+    
+if __name__ == "__main__":
+    loader = DataLoader()
---
+++ utils/generators.py
@@ -0,0 +1,107 @@
+import tensorflow as tf
+
+class RGB_Image_Generator(tf.keras.utils.Sequence) :
+  
+    def __init__(self, obs_ids, labels, batch_size) :
+        self.obs_ids = obs_ids
+        self.labels = labels
+        self.batch_size = batch_size
+        
+        # to make the generator thread safe 
+        self.lock = threading.Lock()
+
+    def __len__(self) :
+        return (np.floor(len(self.obs_ids) / float(self.batch_size))).astype(int)
+  
+    # returns one batch
+    def __getitem__(self, idx) :
+        X_batch = list()
+        y_batch = list()
+
+        for i in range(idx * self.batch_size, (idx+1) * self.batch_size):
+            if i >= len(self.obs_ids): break
+            
+            patch = load_patch(self.obs_ids[i], DATA_PATH, data='rgb')
+            X_batch.append(patch[0])
+            y_batch.append(self.labels[i])
+
+        with self.lock:
+            return np.asarray(X_batch), np.array(y_batch)
+        
+class Full_Satellite_Generator(tf.keras.utils.Sequence) :
+  
+    def __init__(self, obs_ids, labels, batch_size) :
+        self.obs_ids = obs_ids
+        self.labels = labels
+        self.batch_size = batch_size
+        
+        # to make the generator thread safe 
+        self.lock = threading.Lock()
+
+    def __len__(self) :
+        return (np.floor(len(self.obs_ids) / float(self.batch_size))).astype(int)
+  
+    # returns one batch
+    def __getitem__(self, idx) :
+        X_batch = list()
+        y_batch = list()
+
+        for i in range(idx * int(self.batch_size/2), (idx+1) * int(self.batch_size/2)):
+            if i >= len(self.obs_ids): break
+            
+            rgb, near_ir, landcover, altitude = load_patch(self.obs_ids[i], DATA_PATH)
+
+            ni = near_ir.reshape(256, 256, 1)
+            lc = landcover.reshape(256, 256, 1)
+            alt = altitude.reshape(256, 256, 1)
+
+            patch = np.concatenate((ni, lc, alt), axis=2)
+            
+            # add rgb as one observation
+            X_batch.append(rgb)
+            y_batch.append(self.labels[i])
+            
+            # rest of data as second observation
+            X_batch.append(patch)
+            y_batch.append(self.labels[i])
+
+        with self.lock:
+            return np.asarray(X_batch), np.array(y_batch)
+        
+class Patches_Generator_CNN(tf.keras.utils.Sequence) :
+  
+    def __init__(self, obs_ids, labels, batch_size) :
+        self.obs_ids = obs_ids
+        self.labels = labels
+        self.batch_size = batch_size
+        
+        # to make the generator thread safe 
+        self.lock = threading.Lock()
+
+    def __len__(self) :
+        return (np.floor(len(self.obs_ids) / float(self.batch_size))).astype(int)
+  
+    # returns one batch
+    def __getitem__(self, idx) :
+        X_batch = list()
+        X_env_batch = list()
+        y_batch = list()
+
+        for i in range(idx * self.batch_size, (idx+1) * self.batch_size):
+            if i >= len(self.obs_ids): break
+            
+            rgb, near_ir, landcover, altitude = load_patch(self.obs_ids[i], DATA_PATH)
+
+            ni = near_ir.reshape(256, 256, 1)
+            lc = landcover.reshape(256, 256, 1)
+            alt = altitude.reshape(256, 256, 1)
+
+            patch = np.concatenate((rgb, ni, lc, alt), axis=2)
+            
+            X_batch.append(patch)
+            y_batch.append(self.labels[i])
+            
+            X_env_batch.append(tabular_train.loc[self.obs_ids[i]].values)
+            
+        with self.lock:
+            return {'input_1': np.asarray(X_batch), 'input_2': np.asarray(X_env_batch)}, np.asarray(np.array(y_batch))
